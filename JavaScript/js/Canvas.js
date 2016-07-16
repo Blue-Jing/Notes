69561c126a67f1c4d78dc208f24979d67de10fad
Blue-Jing <1147859971@qq.com>
2016-07-16 18:06:28 +0800

    初识 Canvas 画布 --- 2016-07-16 18:06

---
+++ JavaScript/js/Canvas.js
@@ -0,0 +1,25 @@
+/**
+ * Canvas 画布
+ */
+
+(function () {
+    'use strict';
+
+    window.CanvasUtil = {
+        test : testFn
+    };
+
+    function testFn(val) {
+        return val;
+    }
+    
+})();
+
+/**
+ * 运行区
+ */
+EventUtil.addEvent(window, 'load', function () {
+
+});
+
+
